24e6102dba13f01a449df5f8ff9568edc823c001
Igor Makarov <igormaka@gmail.com>
2020-02-02 13:04:42 +0200

    test all the shards, sequentially

--- bench-random.rb
+++ bench-random.rb
@@ -6,7 +6,11 @@ require 'benchmark'
 require 'fileutils'
 
 sources = all_sources
-sample_pods = sources.first.pods.sample(100).sort
+metadata = sources.first.metadata
+sample_pods = sources.first.pods.group_by { |pod|
+  metadata.path_fragment(pod)[0..-2]
+}.sort.map { |shard, pods| pods.first }
+
 
 all_sources.each do |source|
   FileUtils.rm_rf(source.specs_dir)
