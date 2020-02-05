220191b8b6726eed5807d75ac7b57a28e400db67
Igor Makarov <igormaka@gmail.com>
2020-02-05 08:39:21 +0200

    benchmark shards for real

--- bench-shards.rb
+++ bench-shards.rb
@@ -1,29 +1,44 @@
 require 'typhoeus'
 require 'concurrent'
+require 'benchmark'
 
 hydra = Typhoeus::Hydra.new
 
 PREFIX = 'https://pods-cdn.me'
 # PREFIX = 'http://localhost:3000'
 
-counters = {}
 shard_components = '0123456789abcdef'.scan(/\w/)
-triples = shard_components.product(shard_components, shard_components).shuffle
+triples = shard_components.product(shard_components, shard_components)
 
-triples.each do |triple|
-  request = Typhoeus::Request.new("#{PREFIX}/all_pods_versions_#{triple.join('_')}.txt", 
-                                  method: :get,         
-                                  http_version: :httpv2_0,
-                                  accept_encoding: 'gzip'
-                                  )
-  request.on_complete do |response|
-    # STDERR.puts "#{a} #{b} #{c}: #{response.code}"
-    counters[response.code] = (counters[response.code] || 0) + 1
-    # require 'pry'; binding.pry if response.code == 0
-  end
-  hydra.queue(request)
+def request(url)
+  request = Typhoeus::Request.new("#{PREFIX}#{url}", 
+                                      method: :get,         
+                                      http_version: :httpv2_0,
+                                      accept_encoding: 'gzip'
+                                      )
 end
 
-hydra.run
+# 50.times.each do
+loop do
+  counters = {}
 
-STDERR.puts counters
+  time = Benchmark.measure do
+    urls = #['/all_pods.txt'] + 
+      triples.shuffle.map { |t| "/all_pods_versions_#{t.join('_')}.txt"}
+    urls.each do |url|
+      request = request(url)
+      request.on_complete do |response|
+        # STDERR.puts "#{url}: #{response.code}"
+        # STDERR.puts "#{triple}: #{response.headers['cache-control']}"
+        counters[response.code] = (counters[response.code] || 0) + 1
+        # require 'pry'; binding.pry if response.code == 0
+      end
+      # request.run
+      hydra.queue(request)
+    end
+  
+    hydra.run
+  end
+  STDERR.puts counters
+  STDERR.puts "#{1000 * time.real / triples.length}ms per req in parallel (#{triples.length} shards)"
+end
